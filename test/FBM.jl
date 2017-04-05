3536750d2ab5623b526595013927669d3a9c79d8
rand5 <jmellnik@gmail.com>
2017-04-05 16:42:09 -0400

    Update FBM.jl

--- test/FBM.jl
+++ test/FBM.jl
@@ -3,7 +3,8 @@ using Base.Test
 
 @test convert(FGN, FBM(0:0.5:10, 0.4)) == FGN(0.757858283255199,0.4)
 
-p = FBM(0:1/2^10:1, 0.4)
+p = FBM(0:.1:.1*100, 0.4)
+q = FBM(0:.1:.1*101, 0.4)
 
 rand(p)
 rand(p, fbm=false)
@@ -11,3 +12,8 @@ rand([p, p])
 rand(p, rtype=:chol)
 rand(p, fbm=false, rtype=:chol)
 rand([p, p], rtype=:chol)
+
+P = autocov(p)
+Q = autocov(q)
+c = chol(P)
+CholUpdate(Q,convert(Array{Float64,2},c))
